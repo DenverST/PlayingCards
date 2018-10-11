ab171f44588baf01e5e5dc10753fe3bc1ef000c2
Denver Stove <dtstove@gmail.com>
2018-10-11 13:09:12 +1100

    Added a swipe to go through cards

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left,.right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
